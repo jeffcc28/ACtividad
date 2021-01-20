c59584f0b63e2f324a6602720fe5bce1f6af835b
@jeffcc28 <jcantill7@gamil.com>
2021-01-20 14:08:11 -0500

    primer commit

---
+++ 02-spa/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+// Importar Servicios
+import { HeroesService } from "./servicios/hereos.service";
+
+// IMPORTACION DE RUTAS
+
+import { APP_ROUTING } from "./app.routes";
+
+// IMPORTACION DE  COMPONENTES
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    HeroesComponent,
+    AboutComponent,
+    HeroeComponent,
+    BuscadorComponent
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTING
+  ],
+  providers: [
+    HeroesService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
